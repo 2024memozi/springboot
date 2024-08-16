8a4a9df4d2c337e9c134726cd9579d3a961dfd0c
hyunjin9603 <hyunjin9603@gmail.com>
2024-08-16 21:59:08 +0900

    Initial commit

---
+++ src/main/java/com/project/memozi/MemoziApplication.java
@@ -0,0 +1,13 @@
+package com.project.memozi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/memozi/MemoziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.memozi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
