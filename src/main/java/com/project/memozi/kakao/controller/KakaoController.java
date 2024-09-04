e2965247de62f27829a1a42c4cbc3d9f97d3d348
hyunjin9603 <hyunjin9603@gmail.com>
2024-09-04 14:43:04 +0900

    fix:Oauth

--- src/main/java/com/project/memozi/kakao/controller/KakaoController.java
+++ src/main/java/com/project/memozi/kakao/controller/KakaoController.java
@@ -22,26 +22,21 @@ public class KakaoController {
     private final KakaoService kakaoService;
     private final CustomOAuth2UserService customOAuth2UserService;
 
-//    @GetMapping("/login/oauth2/code/kakao")
-//    public ResponseEntity<String> callback (@RequestParam("code") String code) {
-//        String accessToken = kakaoService.getAccessTokenFromKakao(code);
-//        Member member = kakaoService.getUserInfo(accessToken);
-//        String jwtToken = kakaoService.generateJwtForUser(member);
-//        return new ResponseEntity<>(jwtToken, HttpStatus.OK);
-//    }
-
-    @PostMapping("/login/oauth2/code/kakao")
-    public ResponseEntity<String> authenticateWithToken(@RequestBody Map<String, String> body) {
-        String accessToken = body.get("accessToken");
-        if (accessToken == null) {
-            return new ResponseEntity<>("accessToken이 필요합니다.", HttpStatus.BAD_REQUEST);
+    @PostMapping("/oauth2/kakao/login")
+    public ResponseEntity<String> handleKakaoLogin(@RequestBody Map<String, String> body){
+        String code = body.get("code");
+        if(code == null){
+            return new ResponseEntity<>("인가코드가 필요합니다.", HttpStatus.BAD_REQUEST);
         }
+
+        String accessToken = kakaoService.getAccessTokenFromKakao(code);
         Member member = kakaoService.getUserInfo(accessToken);
         String jwtToken = kakaoService.generateJwtForUser(member);
+
         return new ResponseEntity<>(jwtToken, HttpStatus.OK);
     }
 
-    @GetMapping("/oauth2/kakao")
+    @GetMapping("/login/oauth2/kakao")
     public void redirectKakaoLogin(HttpServletResponse response) throws IOException {
         String kakaoAuthUrl = kakaoService.getKakaoAuthUrl();
         response.sendRedirect(kakaoAuthUrl);
